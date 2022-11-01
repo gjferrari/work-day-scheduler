b910f2d447e8d734a439b8f3f5ed1acd2a401948
Genevieve Ferrari <genferrari@gmail.com>
2022-10-31 20:32:57 -0700

    changed local storage code

--- Assets/script.js
+++ Assets/script.js
@@ -36,26 +36,36 @@ $(document).ready(function () {
   }
 
   // CLICK EVENTS FOR EACH BUTTON
+  var clickInput = JSON.parse(localStorage.getItem("message"));
   saveBtnEl.on("click", function () {
-    var oldData = localStorage.getItem("daynotes");
-    var parsedOldData = JSON.parse(oldData);
+    var dayNotes = $(this).siblings(".notes").val();
+    var timeNotes = $(this).parent().attr("id");
+
+    var clickedInput = dayNotes.add(timeNotes);
+    localStorage.setItem("message", JSON.stringify(clickInput));
+
+    // localStorage.setItem(timeNotes, dayNotes);
+    // localStorage.setItem("timeNotes", timeNotes);
+
+    // var oldData = localStorage.getItem("daynotes");
+    // var parsedOldData = JSON.parse(oldData);
     // var dayNotes = $(this).siblings("#text8").val();
-    var clickedDayNote = $(this).siblings()[0].innerHTML;
-    var dayNotes = $(".notes");
+    // var clickedDayNote = $(this).siblings()[0].innerHTML;
+    // var dayNotes = $(".notes");
     //HOW DO I WRITE A FOR LOOP FOR THIS!!!
     // console.log(clickedDayNote);
     // console.log(dayNotes[0]);
 
     // console.log($(dayNotes[0]).val());
-    parsedOldData[clickedDayNote] = dayNotes[0];
+    // parsedOldData[clickedDayNote] = dayNotes[0];
     // var data = {
     //   "8:00": $(dayNotes[0]).val(),
     // };
 
-    var stringyfiedData = JSON.stringify([parsedOldData]);
-    console.log(stringyfiedData);
+    // var stringyfiedData = JSON.stringify([parsedOldData]);
+    // console.log(stringyfiedData);
 
-    localStorage.setItem("daynotes", stringyfiedData);
+    // localStorage.setItem("daynotes", stringyfiedData);
   });
 
   //   $("#text8").val(localStorage.getItem("daynotes")); //currently saves to all blocks but success!
@@ -66,3 +76,4 @@ $(document).ready(function () {
 // https://remysharp.com/2007/04/12/jquerys-this-demystified/
 // https://frontend.turing.edu/lessons/module-2/introduction-to-jquery.html?ads_cmpid=6451354298&ads_adid=76255849919&ads_matchtype=&ads_network=g&ads_creative=517671727591&utm_term=&ads_targetid=dsa-19959388920&utm_campaign=&utm_source=adwords&utm_medium=ppc&ttv=2&gclid=Cj0KCQjwwfiaBhC7ARIsAGvcPe7NRtPLCAj25JhQPu52LllKMpdd-U2rU3BCB_4b6zkOhrrNY7B3T80aAgpCEALw_wcB
 // https://www.geeksforgeeks.org/moment-js-moment-hour-method/#:~:text=The%20moment().,or%20to%20set%20the%20hours.&text=or-,moment,hours()%3B
+// https://stackoverflow.com/questions/7261431/adding-items-to-an-object-through-the-push-method
