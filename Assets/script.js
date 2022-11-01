6456645e1f2a5c0f14fe88ab53b6586f0a1ad8e3
Genevieve Ferrari <genferrari@gmail.com>
2022-10-31 21:59:30 -0700

    removed unnecessary code & added relevant psuedo code

--- Assets/script.js
+++ Assets/script.js
@@ -36,41 +36,12 @@ $(document).ready(function () {
   }
 
   // CLICK EVENTS FOR EACH BUTTON
-//   var clickInput = JSON.parse(localStorage.getItem("message"));
+
   saveBtnEl.on("click", function () {
     var dayNotes = $(this).siblings(".notes").val();
     var timeNotes = $(this).parent().attr("id");
 
     localStorage.setItem(timeNotes, dayNotes);
-
-    // clickInput.push({
-    //   notes: dayNotes,
-    //   time: timeNotes,
-    // });
-    // localStorage.setItem("message", JSON.stringify(clickInput));
-
-    // localStorage.setItem(timeNotes, dayNotes);
-    // localStorage.setItem("timeNotes", timeNotes);
-
-    // var oldData = localStorage.getItem("daynotes");
-    // var parsedOldData = JSON.parse(oldData);
-    // var dayNotes = $(this).siblings("#text8").val();
-    // var clickedDayNote = $(this).siblings()[0].innerHTML;
-    // var dayNotes = $(".notes");
-    //HOW DO I WRITE A FOR LOOP FOR THIS!!!
-    // console.log(clickedDayNote);
-    // console.log(dayNotes[0]);
-
-    // console.log($(dayNotes[0]).val());
-    // parsedOldData[clickedDayNote] = dayNotes[0];
-    // var data = {
-    //   "8:00": $(dayNotes[0]).val(),
-    // };
-
-    // var stringyfiedData = JSON.stringify([parsedOldData]);
-    // console.log(stringyfiedData);
-
-    // localStorage.setItem("daynotes", stringyfiedData);
   });
   $("#text8").val(localStorage.getItem("8hour"));
   $("#text9").val(localStorage.getItem("9hour"));
@@ -83,10 +54,14 @@ $(document).ready(function () {
   $("#text16").val(localStorage.getItem("16hour"));
   $("#text17").val(localStorage.getItem("17hour"));
 
-  //   $("#text8").val(localStorage.getItem("daynotes")); //currently saves to all blocks but success!
-
   timeofDay(); //reruns the time day function
 });
+
+//PLANS FOR NEXT DRAFT: would like to streamline click event:
+//Is it possible to turn dayNotes & timeNotes into an array then write a for loop for the hour divs?
+//Can I use .each(function) or .push ?
+
+//RESOURCES:
 
 // https://remysharp.com/2007/04/12/jquerys-this-demystified/
 // https://frontend.turing.edu/lessons/module-2/introduction-to-jquery.html?ads_cmpid=6451354298&ads_adid=76255849919&ads_matchtype=&ads_network=g&ads_creative=517671727591&utm_term=&ads_targetid=dsa-19959388920&utm_campaign=&utm_source=adwords&utm_medium=ppc&ttv=2&gclid=Cj0KCQjwwfiaBhC7ARIsAGvcPe7NRtPLCAj25JhQPu52LllKMpdd-U2rU3BCB_4b6zkOhrrNY7B3T80aAgpCEALw_wcB
